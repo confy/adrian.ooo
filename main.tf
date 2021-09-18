c17bc37c85f66eeddc93db70c19d046280068361
Adrian McFarlane <confuzedfx@gmail.com>
2021-09-18 14:40:57 -0700

    correct arn - add region

--- main.tf
+++ main.tf
@@ -9,10 +9,11 @@ terraform {
 
 module "hugosite" {
   source              = "github.com/fillup/terraform-hugo-s3-cloudfront"
+  aws_region          = "us-east-1"
   aliases             = ["www.adrian.ooo", "adrian.ooo"]
   bucket_name         = "www.adrian.ooo"
   cert_domain         = "*.adrian.ooo"
-  deployment_user_arn = "arn:aws:iam::111122223333:person"
+  deployment_user_arn = "arn:aws:iam::526286878664:user/adrian-ooo"
   default_root_object = null
   error_document      = "index.html"
   custom_error_response = [
