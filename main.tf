c9463288a2cb7f0e97402f83562404d7fc150557
Adrian McFarlane <confuzedfx@gmail.com>
2021-09-18 14:33:40 -0700

    add deployment action and base terraform config

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    organization = "confy"
+    workspaces {
+      name = "adrian-ooo"
+    }
+  }
+}
+
+module "hugosite" {
+  source              = "github.com/fillup/terraform-hugo-s3-cloudfront"
+  aliases             = ["www.adrian.ooo", "adrian.ooo"]
+  bucket_name         = "www.adrian.ooo"
+  cert_domain         = "*.adrian.ooo"
+  deployment_user_arn = "arn:aws:iam::111122223333:person"
+  default_root_object = null
+  error_document      = "index.html"
+  custom_error_response = [
+      {
+        error_code         = 404
+        response_code      = 200
+        response_page_path = "/index.html"
+      },
+    ]
+}
