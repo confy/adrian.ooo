f44e3bb25cedfe345e8d7a8957827f2cb5547409
Adrian McFarlane <confuzedfx@gmail.com>
2021-09-19 14:30:31 -0700

    remove action and terraform for netlify

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "confy"
-    workspaces {
-      name = "adrian-ooo"
-    }
-  }
-}
--- terraform/providers.tf
+++
@@ -1,5 +0,0 @@
-#Change profile to the AWS credentials profile you want to use.
-provider "aws" {
-  region  = var.aws_region
-  profile = "fillupio"
-}
